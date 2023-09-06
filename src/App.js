f6bb10976115e75e3a2845ea62fc5704a82cd5bc
Giselle <rodriguezdelgadogiselle@gmail.com>
2023-09-06 14:00:02 -0400

    Cart, MealItem, MealItemForm, AvailableMeals,Meals, MealsSummary Card Input and Modal components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import { Fragment, useState } from "react";
 import Header from "./componets/Layout/Header";
+import Meals from "./componets/Meals/Meals";
+import Cart from "./componets/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <React.Fragment>
-      <Header></Header>
-    </React.Fragment>
+    <Fragment>
+      {cartIsShown && <Cart onHideCart={hideCartHandler}></Cart>}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
--- src/componets/Layout/HeaderCartButton.js
+++ src/componets/Layout/HeaderCartButton.js
@@ -3,8 +3,9 @@ import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = (props) => {
+  
   return (
-    <button className={classes.button}>
+    <button onClick={props.onClick} className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
